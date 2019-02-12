3ce1695cdba294ef71ff2acdb72193a6fc188b41
Hector <dreamallica@gmail.com>
2019-02-12 02:00:55 -0300

    implementing paddle movement

---
+++ Assets/Scripts/gameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameManager : MonoBehaviour
+{
+    public GameObject paddle1;
+
+    public GameObject ball;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
